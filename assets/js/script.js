29e68bbb57eae5bcd36cdef31e612a6665c2c86b
bencheee <sandro.bencinic@gmail.com>
2021-04-10 11:12:20 +0000

    Add addCard function which stores card string to players / dealers hand, calculates numeric value and updates html file with card element

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,13 @@
-let cardsDrawn = [];
+let cardsDrawn = [],
+    playerHandString = [],
+    playerHandValue = [],
+    dealerHandString = [],
+    dealerHandValue = [];
 
 
 $('#bet').click(function () {
     generateCard();
+    addCard(playerHandString, playerHandValue, $('#player-cards'));
 }); // end event listener
 
 
@@ -11,7 +16,6 @@ function generateCard() {
 
     /* Determines card value by generating string
        2-10 or A, J, Q, K */
-
     let randNumber = Math.ceil(Math.random() * 13);
     switch (randNumber) {
         case 1:
@@ -32,7 +36,6 @@ function generateCard() {
 
     /* Determines card color by generating string
        H, C, D or S (first letter of the color) */
-
     let randColor = Math.ceil(Math.random() * 4);
     switch (randColor) {
         case 2:
@@ -50,10 +53,47 @@ function generateCard() {
 
     /* If generated combination of strings already exists
        in cardsDrawn array generate another*/
-
     if (!cardsDrawn.includes(`${cardValue}${cardColor}`)) {
         cardsDrawn.unshift(`${cardValue}${cardColor}`);
     } else {
         generateCard();
     };
-};
+}; // end generateCard
+
+
+function addCard(handString, handValue, container) {
+
+    // Creates and adds new card element to index.html
+    let card = $('<div></div>').addClass('card');
+    card.css('background-image', `url(/images/${cardsDrawn[0]}.jpg)`);
+    container.append(card);
+
+    // Updates handString array for player / dealer with string value of drawn card
+    handString.unshift(cardsDrawn[0]);
+
+    /*
+    Converts first character of card string to number
+    Ace card is converted to NaN
+    */
+    let convertedValue;
+    let firstChar = handString[0].slice(0, 1);
+    switch (firstChar) {
+        case 'J':
+            convertedValue = 10;
+            break;
+        case 'Q':
+            convertedValue = 10;
+            break;
+        case 'K':
+            convertedValue = 10;
+            break;
+        case '1':
+            convertedValue = 10;
+            break;
+        default:
+            convertedValue = Number(firstChar);
+    };
+
+    // Updates handValue array for player / dealer with numeric value of card drawn
+    handValue.unshift(convertedValue);
+}; // end addCard
