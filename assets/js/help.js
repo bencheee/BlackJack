d1cfb8d319ca9030f0f21acb2ac6dc47ea6987f3
bencheee <sandro.bencinic@gmail.com>
2021-05-01 09:46:40 +0000

    Add content to help section

--- assets/js/help.js
+++ assets/js/help.js
@@ -1,30 +1,34 @@
 let help = [{
-        title: `Title 1`,
-        text: `Example Text 1`
+        title: `BASIC RULES`,
+        text: `The main objective of the game is to have greater score than the dealer at the end of each round while the score must not exceed the total of 21. In case the score is greater than 21 at any point, this results with "bust" and the round is lost.
+        In case the score in exactly 21, this results with "Blackjack". If the dealer's score is also 21, the round ends in a tie. Otherwise the player wins the round.`
     },
     {
-        title: `Title 2`,
-        text: `Example Text 2`
+        title: `BETTING`,
+        text: `The player starts the game with the amount of €1000 in the bank. Before the start of each round, the player has to place the initial bet. In case the round is won, the award is paid to the player’s bank in 3/2 ratio (example: If the bet is €100, the payout to the bank is €150.). If the round ends in a tie, the whole bet is returned to the player’s bank.`
     },
     {
-        title: `Title 3`,
-        text: `Example Text 3`
+        title: `INITIAL DEAL`,
+        text: `At the start of each round two cards are dealt to the player and the dealer. The dealer’s first card remains hidden until it’s the dealer’s turn to play. In case the player does not have Blackjack, the additional options become available (“Hit”, “Stand” or “Double”).`
     },
     {
-        title: `Title 4`,
-        text: `Example Text 4`
+        title: `HIT/STAND`,
+        text: `By pressing the “Hit” button, a new card is added to the player’s hand. The player can draw new cards until there is maximum of 5 cards in the player’s hand or until the total score exceeds 21 which ends with “bust”. By pressing the “Stand” button, the player ends the turn and the dealer is on the move.`
     },
     {
-        title: `Title 5`,
-        text: `Example Text 5`
+        title: `DOUBLE`,
+        text: `By pressing the “Double” button, the amount of the original bet is doubled. The player can draw only one new card after and in case the game does not end with bust, it is the dealer’s turn to play.
+        In case the round is won, the award is paid to the player’s bank in 2/1 ratio (example: If the bet is €100, the payout to the bank is €200.). If the round ends in a tie, the whole bet is returned to the player’s bank.`
     },
     {
-        title: `Title 6`,
-        text: `Example Text 6`
+        title: `INSURANCE`,
+        text: `Insurance gives an option to the player to bet on whether the dealer has Blackjack in his initial hand. This option is activated in case if the only visible card in the dealer’s hand is an ace or value of ten.
+        This bet is separate from the original bet. In case insurance bet is won, player loses the original bet. The insurance payout is paid to the player’s bank in 3/2 ratio (example: If the bet is €100, the payout to the bank is €150.).
+        In case the dealer does not have Blackjack, the player loses the insurance bet but the game continues.`
     },
     {
-        title: `Title 7`,
-        text: `Example Text 7`
+        title: `END OF GAME`,
+        text: `The player can “Cash-out” at any time which ends the game. In case the amount in the player’s bank drops to €0, the game is automatically lost.`
     }
 ];
 
