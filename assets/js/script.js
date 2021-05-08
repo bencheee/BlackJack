1d1272b594605a1b3d3f35a3298118e39c13c4f2
bencheee <sandro.bencinic@gmail.com>
2021-05-08 10:16:51 +0000

    Fix typo in pop up message uftar pressing stand button

--- assets/js/script.js
+++ assets/js/script.js
@@ -231,7 +231,7 @@ $('#hit').click(function () {
 
 $('#stand').click(function () {
 
-    popUpOn(`You have ${playerScore} points in your hand! It's dealer's turn now!`);
+    popUpOn(`You have ${playerScore} points! It's dealer's turn now!`);
 
     $('.pop-up-box button').click(function () {
         popUpOff();
